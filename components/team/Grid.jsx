bd25217fc37bf1dcb6ff3013f645bc23ba25f119
UtkarshBajpai-1929 <utkarshbajpai632@gmail.com>
2026-09-02 16:09:11 +0530

    team filters added

--- components/team/Grid.jsx
+++ components/team/Grid.jsx
@@ -1,11 +1,40 @@
 import Card from "./Card";
 
 export default function Grid({ members }) {
+  const teams = [
+     "convener",
+    "secretariat",
+    "coordinators",
+   
+    "pr",
+    "event",
+    "sponsorship",
+    "design",
+  ];
+
   return (
-    <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4">
-      {members.map((member) => (
-        <Card key={member.name} member={member} />
-      ))}
+    <div className="space-y-16">
+      {teams.map((team) => {
+        const teamMembers = members.filter(
+          (member) => member.team === team
+        );
+
+        if (teamMembers.length === 0) return null;
+
+        return (
+          <section key={team}>
+            <h2 className="mb-4 text-2xl font-bold uppercase">
+              {team} Team
+            </h2>
+
+            <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4">
+              {teamMembers.map((member) => (
+                <Card key={member.name} member={member} />
+              ))}
+            </div>
+          </section>
+        );
+      })}
     </div>
   );
 }
